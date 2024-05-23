6ad9635e13a3c5d60969a67d2845cd6eb446dd1f
Devon <devnathan94@gmail.com>
2024-05-23 09:22:09 +0300

    feat: DFSOnBST

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,18 +1,26 @@
+function search(curr: BinaryNode<number> | null, needle: number): boolean {
+    if (!curr) return false;
+
+    if (curr.value === needle) return true;
+
+    if (curr.value > needle) return search(curr.left, needle);
+    return search(curr.right, needle);
+}
+
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
-    if (head.value === needle) return true;
-
-    if (head.value > needle) {
-        if (head.left) {
-            return dfs(head.left, needle);
-        }
-        return false;
-    }
-
-    if (head.value < needle) {
-        if (head.right) {
-            return dfs(head.right, needle);
-        }
-        return false;
-    }
-    return false;
+    return search(head, needle);
+    // if (head.value === needle) return true;
+    // if (head.value > needle) {
+    //     if (head.left) {
+    //         return dfs(head.left, needle);
+    //     }
+    //     return false;
+    // }
+    // if (head.value < needle) {
+    //     if (head.right) {
+    //         return dfs(head.right, needle);
+    //     }
+    //     return false;
+    // }
+    // return false;
 }
